fd564c1c1954b8950803bb76723fb651ddd5d0c1
XuHanpeace <779372053@qq.com>
2017-12-02 15:25:26 +0800

    add vueChatServer

---
+++ README.md
@@ -0,0 +1,2 @@
+## vueChatServer
+> 这是一个基于express的微型web server,利用了socket.io实现了websocket接受消息和广播消息的功能
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('C:/Users/xuahan/Desktop/www/dist'))
+
+const server = app.listen(1231)
+
+//add websocket
+//reference from https://socket.io/docs/server-api/
+const io = require('socket.io')(server)
+
+io.sockets.on('connection', socket => {
+
+    socket.on('sendMsg', data => {
+
+        //将收到的消息广播给其他用户
+        socket.broadcast.emit('newMsg', data)
+
+        //向所有用户推送实时消息
+        var timeObj = new Date(),
+            time = timeObj.getHours() + ':' + timeObj.getMinutes(),
+            postObj = {
+                content: data.userName + ': ' + data.content,
+                time: time
+            }
+
+        io.sockets.emit('postMsg', postObj)
+    })
+})
+
+
